7517784af28c01ccbf18f5baa126b58a1d329a74
alexgomezdev <byrambo166@gmail.com>
2022-04-20 02:12:32 +0200

    ADD recursive function for all fetch to marvel comics

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,24 +1,44 @@
     
     
     
-    
-    $("#compareBtn").click(function(){
-    
-        $.get('https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=1a3866a39a0a5d92dc712572898c21b1&hash=5cf818d1edaa8819ac48ceea00b736e9', function(data){
-            
+    function recursiveAjax($offset = 0){
+        
+        $.get('https://gateway.marvel.com:443/v1/public/comics?offset='+$offset+'ts=1&apikey=1a3866a39a0a5d92dc712572898c21b1&hash=5cf818d1edaa8819ac48ceea00b736e9', function(data){
             var marvelComics=data.data;
-            console.log(marvelComics);
-            $.ajax({ 
-                type: "POST",
-                headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                url: `compareComics`,
-                data: JSON.stringify(marvelComics),    
-                contentType: "application/json",
-                dataType: "json",
-                success: function(){
-                    console.log('Todo good')
-                }
-            })
+            if(marvelComics.total-$offset<100){
+                $.ajax({ 
+                    type: "POST",
+                    headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                    url: `compareComics`,
+                    data: JSON.stringify(marvelComics),    
+                    contentType: "application/json",
+                    dataType: "json",
+                    success: function(){
+                        console.log('Todo good')
+                    }
+                })
+                return $cont;
+            }else{
+                $.ajax({ 
+                    type: "POST",
+                    headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                    url: `compareComics`,
+                    data: JSON.stringify(marvelComics),    
+                    contentType: "application/json",
+                    dataType: "json",
+                    success: function(){
+                        console.log('Todo good')
+                    }
+                }) 
+               recursiveAjax($offset + 100,) 
+            }
+    
+         
            ;;
         } )    
+        
+    }
+    $("#compareBtn").click(function(){
+        recursiveAjax();
         })
+  
